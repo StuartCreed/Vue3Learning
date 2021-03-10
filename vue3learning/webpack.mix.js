39d607fdf4abc0cf1279d966b95e98ca6968983c
Stuart Creed <stuart.a.creed@gmail.com>
2021-03-10 11:31:31 +0000

    Initial commit

--- vue3learning/webpack.mix.js
+++ vue3learning/webpack.mix.js
@@ -11,8 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').vue()
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ])
-    .browserSync('http://localhost:8081/')
+mix.js('resources/js/app.js', 'public/js').vue();
+    // .postCss('resources/css/app.css', 'public/css', [
+    //     //
+    // ])
+mix.browserSync('http://localhost:8081/')
